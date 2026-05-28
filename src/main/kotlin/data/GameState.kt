74aed6553ca88c9cbac2c3a3c0ab15152be25863
pacjo <korycinski.kam@gmail.com>
2026-05-28 13:12:51 +0200

    add code written during interview

--- src/main/kotlin/data/GameState.kt
+++ src/main/kotlin/data/GameState.kt
@@ -9,7 +9,8 @@ data class GameState(
     val config: GameConfig = GameConfig(),
     val board: Board = Board(config.rows, config.cols),
     val status: GameStatus = GameStatus.InProgress("Player 1"),
-    val currentPlayer: Int = 1
+    val currentPlayer: Int = 1,
+    private val moveHistory: MutableList<Int> = mutableListOf()
 ) {
     fun dropPiece(col: Int): GameState {
         // don't allow moves after win/draw
@@ -19,6 +20,9 @@ data class GameState(
         // find first empty row or return early (so no action takes place)
         val row = (config.rows - 1 downTo 0)
             .firstOrNull { board[it, col] == Cell.NONE } ?: return this
+
+        // update history
+        moveHistory.add(col)
 
         val newBoard = board.copy()
         val playerCell = if (currentPlayer == 1) Cell.RED else Cell.YELLOW
@@ -45,6 +49,30 @@ data class GameState(
             board = newBoard,
             status = newStatus,
             currentPlayer = newPlayer
+        )
+    }
+
+    // unused as requested during interview
+    fun undoMove(): GameState {
+        // this should also check for an empty list, since .removeLast() can throw an exception
+        // but interviewer after mentioning it said there's no need to add that
+        // and we've finished a couple of minutes before the end of the interview time slot
+
+        // find the row in which the last piece landed
+        val col = moveHistory.removeLast()
+        val newBoard = board.copy()
+        val rowToRemove = (0 until config.rows).first { row ->
+            newBoard[row, col] != Cell.NONE
+        }
+
+        // undo by setting to empty
+        newBoard[rowToRemove, col] = Cell.NONE
+
+        // and return (don't forget to change player)
+        return copy(
+            board = newBoard,
+            status = status,
+            currentPlayer = 3 - currentPlayer
         )
     }
 
